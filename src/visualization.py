baab0d270cd5717d3a14ccbf7cbe6ea2eb69ecf7
Charles <charles.wilmot2@hotmail.fr>
2020-07-20 11:30:53 +0200

    second ax for visualization

--- src/visualization.py
+++ src/visualization.py
@@ -6,24 +6,47 @@ plt.ion()
 class Visualization(object):
     def __init__(self, mini, maxi):
         self._fig = plt.figure()
-        self._critic_prediction_ax = self._fig.add_subplot(111)
+        self._critic_prediction_ax = self._fig.add_subplot(121)
         self._critic_prediction_ax.set_ylim([mini, maxi])
-        self._target_line = None
+        self._policy_prediction_ax = self._fig.add_subplot(122)
+        self._policy_prediction_ax.set_ylim([mini, maxi])
+        self._critic_target_line = None
+        self._policy_target_line = None
+        self._shown = False
 
-    def __call__(self, target, prediction):
-        if self._target_line is None:
-            self._target_line, = self._critic_prediction_ax.plot(
+    def update_critic(self, target, prediction):
+        if self._critic_target_line is None:
+            self._critic_target_line, = self._critic_prediction_ax.plot(
                 target, label='target'
             )
-            self._prediction_line, = self._critic_prediction_ax.plot(
+            self._critic_prediction_line, = self._critic_prediction_ax.plot(
                 prediction, label='prediction'
             )
             self._critic_prediction_ax.legend()
-            self._fig.show()
+            if not self._shown:
+                self._fig.show()
             self._fig.canvas.flush_events()
         else:
-            self._target_line.set_ydata(target)
-            self._prediction_line.set_ydata(prediction)
+            self._critic_target_line.set_ydata(target)
+            self._critic_prediction_line.set_ydata(prediction)
+            self._fig.canvas.draw()
+            self._fig.canvas.flush_events()
+
+    def update_policy(self, target, prediction):
+        if self._policy_target_line is None:
+            self._policy_target_line, = self._policy_prediction_ax.plot(
+                target, label='target'
+            )
+            self._policy_prediction_line, = self._policy_prediction_ax.plot(
+                prediction, label='prediction'
+            )
+            self._policy_prediction_ax.legend()
+            if not self._shown:
+                self._fig.show()
+            self._fig.canvas.flush_events()
+        else:
+            self._policy_target_line.set_ydata(target)
+            self._policy_prediction_line.set_ydata(prediction)
             self._fig.canvas.draw()
             self._fig.canvas.flush_events()
 
