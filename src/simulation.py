41d6df550ef556b0bb522d7232d49c53e52084c7
Charles <charles.wilmot2@hotmail.fr>
2020-07-20 11:28:00 +0200

    fix and comment decorators

--- src/simulation.py
+++ src/simulation.py
@@ -30,6 +30,8 @@ def communicate_return_value(method):
 
 
 def default_dont_communicate_return(cls):
+    """Class decorator for the SimulationConsumers meaning that by default, all
+    methods don't communicate their return value to the Producer class"""
     for attribute_name, attribute in cls.__dict__.items():
         if callable(attribute):
             communicate = hasattr(attribute, '_communicate_return_value')
@@ -38,20 +40,22 @@ def default_dont_communicate_return(cls):
 
 
 def c2p_convertion_function(cls, method):
-    def new_method(self, *args, **kwargs):
+    """Function that transform a Consumer method into a Producer method.
+    It add a blocking flag that determines whether the call is blocking or not.
+    If you call a `Producer.mothod(blocking=False)`, you then must
+    `Producer._wait_for_answer()`"""
+    def new_method(self, *args, blocking=True, **kwargs):
         cls._send_command(self, method, *args, **kwargs)
-        if method._communicate_return_value:
-            # print(method, "waiting for an answer...")
-            while not self._process_io["return_value_pipe_out"].poll(1):
-                # print(method, "waiting for an answer...nothing yet...alive?")
-                self._check_consumer_alive()
-            answer = self._process_io["return_value_pipe_out"].recv()
-            # print(method, "waiting for an answer...got it!")
-            return answer
+        if method._communicate_return_value and blocking:
+            return cls._wait_for_answer(self)
+    new_method._communicate_return_value = method._communicate_return_value
     return new_method
 
 
 def consumer_to_producer_method_conversion(cls):
+    """Class decorator that transforms all methods from the Consumer to the
+    Producer, except for methods starting with an '_', and for the
+    multiprocessing.Process methods"""
     proc_methods = [
         "run", "is_alive", "join", "kill", "start", "terminate", "close"
     ]
@@ -73,28 +77,33 @@ def consumer_to_producer_method_conversion(cls):
 
 
 def p2p_convertion_function(name):
+    """This function transforms a producer method into a Pool method"""
     def new_method(self, *args, **kwargs):
         if self._distribute_args_mode:
             # all args are iterables that must be distributed to each producer
-            return [
+            for i, producer in enumerate(self._active_producers):
                 getattr(producer, name)(
                     *[arg[i] for arg in args],
+                    blocking=False,
                     **{key: value[i] for key, value in kwargs.items()}
                 )
-                for i, producer in enumerate(self._active_producers)
-            ]
         else:
+            for producer in self._active_producers:
+                getattr(producer, name)(*args, blocking=False, **kwargs)
+        if getattr(SimulationProducer, name)._communicate_return_value:
             return [
-                getattr(producer, name)(*args, **kwargs)
-                for producer in self._active_producers
+                producer._wait_for_answer() for producer in self._active_producers
             ]
     return new_method
 
 def producer_to_pool_method_convertion(cls):
+    """This class decorator transforms all Producer methods (besides close and
+    methods starting with '_') to the Pool object."""
     convertables = {
         method_name: method \
         for method_name, method in SimulationProducer.__dict__.items()\
-        if callable(method) and not method_name.startswith("_")
+        if callable(method) and not method_name.startswith("_")\
+        and not method_name == 'close'
     }
     for method_name, method in convertables.items():
         new_method = p2p_convertion_function(method_name)
