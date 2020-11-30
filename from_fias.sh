205b46b23527737b0c4facfe78a44e6518719a55
Charles <charles.wilmot2@hotmail.fr>
2020-11-30 13:11:45 +0100

    no confirmation

--- from_fias.sh
+++ from_fias.sh
@@ -1,13 +1,13 @@
 # from fias to cx1g8:
 # copy only the experiment folder (only recent files)
-rsync -anvzr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/aec-tf-v2/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
-
-echo    # (optional) move to a new line
-echo    # (optional) move to a new line
-read -p "Do you want to proceed? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+# rsync -anvzr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/aec-tf-v2/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
+# 
+# echo    # (optional) move to a new line
+# echo    # (optional) move to a new line
+# read -p "Do you want to proceed? " -n 1 -r
+# echo    # (optional) move to a new line
+# if [[ ! $REPLY =~ ^[Yy]$ ]]
+# then
+#     exit 1
+# fi
 rsync -azPr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/coppelia_sim_inverse_model/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
--- to_fias.sh
+++ to_fias.sh
@@ -1,13 +1,13 @@
 # from cx1g8 to fias:
 # copy everything but the experiment dir
-rsync -anv --delete --exclude=experiments/* --exclude=.git/* ~/Code/coppelia_sim_inverse_model/ wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model
+# rsync -anv --delete --exclude=experiments/* --exclude=.git/* ~/Code/coppelia_sim_inverse_model/ wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model
 
-echo    # (optional) move to a new line
-echo    # (optional) move to a new line
-read -p "Do you want to proceed? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+# echo    # (optional) move to a new line
+# echo    # (optional) move to a new line
+# read -p "Do you want to proceed? " -n 1 -r
+# echo    # (optional) move to a new line
+# if [[ ! $REPLY =~ ^[Yy]$ ]]
+# then
+#     exit 1
+# fi
 rsync -a --delete --exclude=experiments/* --exclude=.git/* ~/Code/coppelia_sim_inverse_model/ wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model
