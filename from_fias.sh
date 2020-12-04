f7e3d9065a75a0e276f3c6196c07d0ec7cf42070
Charles <charles.wilmot2@hotmail.fr>
2020-12-04 18:06:36 +0100

    improved rsync

--- from_fias.sh
+++ from_fias.sh
@@ -1,7 +1,7 @@
 # from fias to cx1g8:
 # copy only the experiment folder (only recent files)
 # rsync -anvzr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/aec-tf-v2/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
-# 
+#
 # echo    # (optional) move to a new line
 # echo    # (optional) move to a new line
 # read -p "Do you want to proceed? " -n 1 -r
@@ -10,4 +10,5 @@
 # then
 #     exit 1
 # fi
-rsync -azPr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/coppelia_sim_inverse_model/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
+# rsync -azPr --delete --include-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/coppelia_sim_inverse_model/experiments/ -mtime -2 -mindepth 1 -printf "%P\n"') --exclude=* --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
+rsync -azPr --delete --exclude-from=<(ssh wilmot@fias.uni-frankfurt.de 'find ~/Documents/code/coppelia_sim_inverse_model/experiments/ -mtime +2 -mindepth 1 -maxdepth 1 -printf "%P\n"') --delete-excluded wilmot@fias.uni-frankfurt.de:~/Documents/code/coppelia_sim_inverse_model/experiments/ ~/Code/coppelia_sim_inverse_model/experiments
