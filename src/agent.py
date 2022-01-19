5bd1bc97de19af619b2486cd38bf1776c6cff0f6
Charles <charles.wilmot2@hotmail.fr>
2022-01-19 15:29:20 +0100

    fix

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from custom_layers import custom_objects, PrimitiveModelEnd
+from custom_layers import custom_objects, PrimitiveModelEnd, BottleneckExploration
 from td3 import TD3
 
 
